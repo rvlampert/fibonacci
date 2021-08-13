049eb21f93e21ceac48808ced355dc17de8fe6bc
Ricardo Lampert <ricardo.lampert@softplan.com.br>
2021-08-13 14:56:09 -0300

    criando projeto fibonacci

---
+++ project/main.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Fibonacci;
+using System.Collections.Generic;
+
+namespace Main
+{
+    
+            
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 4;
+            List<int> sequence = new List<int>();
+            Fibonacci_iterative fib = new Fibonacci_iterative();
+            sequence = fib.fibonacci_serie(index);
+            Console.Write("Série: ");
+            foreach (int value in sequence){
+                Console.Write("|"+value);
+            }
+            Console.WriteLine("|");
+            int final_value = fib.fibonacci_value(index);
+            Console.WriteLine("Valor final: "+final_value);
+
+        }
+    }
+}
